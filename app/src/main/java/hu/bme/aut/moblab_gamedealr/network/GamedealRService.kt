5a598793de25241067ac12cdf8249032e9b38076
Katalin Dóra Szabó <68538708+szkatica22@users.noreply.github.com>
2023-05-14 17:08:44 +0200

    Write functions in GamedealRService

--- app/src/main/java/hu/bme/aut/moblab_gamedealr/network/GamedealRService.kt
+++ app/src/main/java/hu/bme/aut/moblab_gamedealr/network/GamedealRService.kt
@@ -1,4 +1,20 @@
 package hu.bme.aut.moblab_gamedealr.network
 
-class GamedealRService {
+import hu.bme.aut.moblab_gamedealr.model.Deal
+import hu.bme.aut.moblab_gamedealr.model.Game
+import hu.bme.aut.moblab_gamedealr.model.Store
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface GamedealRService {
+
+    @GET("games")
+    fun searchGames(@Query("title") title: String): Call<List<Game>>
+
+    @GET("stores")
+    fun getStores(): Call<List<Store>>
+
+    @GET("deals")
+    fun getDeals(@Query("title") title: String): Call<List<Deal>>
 }
