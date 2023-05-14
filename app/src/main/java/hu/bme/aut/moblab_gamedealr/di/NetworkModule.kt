0de26ce6d97a1f874c2a9aff9f8becec9977741e
Katalin Dóra Szabó <68538708+szkatica22@users.noreply.github.com>
2023-05-14 16:28:54 +0200

    Imports and create NetworkModule
    Import Retrofit and Gson converters

--- app/src/main/java/hu/bme/aut/moblab_gamedealr/di/NetworkModule.kt
+++ app/src/main/java/hu/bme/aut/moblab_gamedealr/di/NetworkModule.kt
@@ -1,4 +1,42 @@
 package hu.bme.aut.moblab_gamedealr.di
 
-class NetworkModule {
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import hu.bme.aut.moblab_gamedealr.network.GamedealRService
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+                //.addInterceptor(RequestInterceptor())
+                .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl(
+                        "https://www.cheapshark.com/api/1.0"
+                )
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideGamedealRService(retrofit: Retrofit): GamedealRService {
+        return retrofit.create(GamedealRService::class.java)
+    }
 }
