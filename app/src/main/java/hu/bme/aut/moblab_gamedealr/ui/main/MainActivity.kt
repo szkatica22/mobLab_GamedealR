57301937ae36e6544a3baf369798e2b09dadb35b
szkatica22 <szkatica22@gmail.com>
2023-05-31 13:10:01 +0200

    Update MainActivity.kt

--- app/src/main/java/hu/bme/aut/moblab_gamedealr/ui/main/MainActivity.kt
+++ app/src/main/java/hu/bme/aut/moblab_gamedealr/ui/main/MainActivity.kt
@@ -7,13 +7,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
 import hu.bme.aut.moblab_gamedealr.ui.theme.MobLab_GamedealRTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Obtain the FirebaseAnalytics instance.
+        firebaseAnalytics = Firebase.analytics
+
         setContent {
             MobLab_GamedealRTheme {
                 // A surface container using the 'background' color from the theme
